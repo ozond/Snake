b32639e2a2c2408451bb70f2e339d89674efa1f9
ozond <ozond435@gmail.com>
2023-05-04 22:52:50 +0200

    snake

--- js/game.js
+++ js/game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById("game-board")
 function main(currentTime) {
     if (gameOver) {
         if (confirm("You lost. Press ok to restart.")) {
-            window.location = "https://ozond.github.io/Snake/"
+            window.location = "/"
         }
         return
     }
